c250e113702fef631a83dedbbbac3e6b0516b852
Dayanch  Muhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2022-07-15 20:13:18 +0300

    Blog

--- config/Routes.go
+++ config/Routes.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	admin "blog/admin/controllers"
+	site "blog/site/controllers"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
@@ -15,7 +16,25 @@ func Routes() *httprouter.Router {
 	r.GET("/admin/delete/:id", admin.Dashboard{}.Delete)
 	r.GET("/admin/edit/:id", admin.Dashboard{}.Edit)
 	r.POST("/admin/update/:id", admin.Dashboard{}.Update)
+
+	//Categories
+	r.GET("/admin/kategoriler", admin.Categories{}.Index)
+	r.POST("/admin/kategoriler/add", admin.Categories{}.Add)
+	r.GET("/admin/kategoriler/delete/:id", admin.Categories{}.Delete)
+
+	//Userops
+	r.GET("/admin/login", admin.Userops{}.Index)
+	r.POST("/admin/do_login", admin.Userops{}.Login)
+	r.GET("/admin/logout", admin.Userops{}.Logout)
+
+	//SITE
+	//Homepage
+	r.GET("/", site.Homepage{}.Index)
+	r.GET("/yazilar/:slug", site.Homepage{}.Detail)
+
+	//Serve File
 	r.ServeFiles("/admin/assets/*filepath", http.Dir("admin/assets"))
+	r.ServeFiles("/assets/*filepath", http.Dir("site/assets"))
 	r.ServeFiles("/uploads/*filepath", http.Dir("uploads"))
 	return r
 }
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	admin_models.Post{}.Migrate()
+	admin_models.User{}.Migrate()
+	admin_models.Category{}.Migrate()
 	http.ListenAndServe(":8080", config.Routes())
 
 }
